a0e7173f131c85ac4c915fbf3f1da7c7165c7e9e
romantrynko <trynkoroman25@gmail.com>
2020-09-23 19:39:44 +0300

    vue_lesson_2

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,19 @@
 <template>
   <div>
-      <h1>Header</h1>
+      <h1 v-highlight='"blue"'>Header</h1>
+      <h1>{{header | toLovercase | toUpercase}}</h1>
   </div>
 </template>
 
 <script>
 export default {
-    name: 'Header'
+    name: 'Header',
+
+    data() {
+      return {
+        header: 'HEEEAADDDEERRRRR!!!!!!!!'
+      }
+    }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -3,6 +3,23 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+Vue.directive('highlight', {
+  bind(el,binding,vnNode) {
+    console.log(el, 'el');
+    console.log(binding, 'binding');
+    console.log(vnNode, 'vnNode');
+    el.style.backgroundColor = binding.value
+  }
+});
+
+Vue.filter('toLovercase', function (value) {
+  return value.toLowercase()
+})
+
+Vue.filter('toUpercase', function (value) {
+  return value.toUppercase()
+})
+
 export const eventBus = new Vue({
   methods: {
     changeFooter(value) {
